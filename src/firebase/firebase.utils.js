478ab47a53237f6656542aab9182c797f8e49e45
Tian9000 <wijayacristian220@gmail.com>
2019-09-09 01:53:50 +0700

    A little Changed

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    apiKey: "AIzaSyAGtXSqVdb0Y6H0faLp1YYMUYHaOeb8MH8",
-    authDomain: "e-commerce2019.firebaseapp.com",
-    databaseURL: "https://e-commerce2019.firebaseio.com",
-    projectId: "e-commerce2019",
-    storageBucket: "",
-    messagingSenderId: "380664865496",
-    appId: "1:380664865496:web:da53a6f85ad1fc0a"
-};
+
 
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
