8c36ef7129ca02698b1c307c545ef67bc4395ddd
Tian9000 <wijayacristian220@gmail.com>
2019-09-09 01:49:10 +0700

    change some files to redux-saga

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-
-// const config = {
-//     apiKey: "AIzaSyAGtXSqVdb0Y6H0faLp1YYMUYHaOeb8MH8",
-//     authDomain: "e-commerce2019.firebaseapp.com",
-//     databaseURL: "https://e-commerce2019.firebaseio.com",
-//     projectId: "e-commerce2019",
-//     storageBucket: "",
-//     messagingSenderId: "380664865496",
-//     appId: "1:380664865496:web:da53a6f85ad1fc0a"
-// };
+const config = {
+    apiKey: "AIzaSyAGtXSqVdb0Y6H0faLp1YYMUYHaOeb8MH8",
+    authDomain: "e-commerce2019.firebaseapp.com",
+    databaseURL: "https://e-commerce2019.firebaseio.com",
+    projectId: "e-commerce2019",
+    storageBucket: "",
+    messagingSenderId: "380664865496",
+    appId: "1:380664865496:web:da53a6f85ad1fc0a"
+};
 
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -77,7 +76,14 @@ export const convertCollectionsSnapshotToMap = (collections) => {
 
 }
 
-
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject)
+  })
+}
 
 
 firebase.initializeApp(config);
@@ -87,9 +93,9 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
 
 
 export default firebase;
