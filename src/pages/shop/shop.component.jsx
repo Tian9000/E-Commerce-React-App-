f2e2ef143e0c9e947d35f5619e0f6b8f9e27931a
Tian9000 <wijayacristian220@gmail.com>
2019-09-10 00:23:29 +0700

    Change Some File to React Hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -8,16 +8,11 @@ import CollectionsOverviewContainer from '../../components/collections-overview/
 import CollectionPageContainer from '../collection/collection.container';
 
 
-  class ShopPage extends React.Component {
-    componentDidMount() {
-      const { fetchCollectionsStart } = this.props;
-      
+  const ShopPage = ({ fetchCollectionsStart, match }) => {
+    useEffect(() => {
       fetchCollectionsStart();
-    }
+    }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-    
     return (
       <div className='shop-page'>
         <Route
@@ -32,7 +27,7 @@ import CollectionPageContainer from '../collection/collection.container';
       </div>
     );
   }
-}
+
 
 const mapDispatchToProps = dispatch => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
